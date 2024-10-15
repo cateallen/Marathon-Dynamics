e8fb9f9ea50607691850804e9a88ba48048d456e
Cate Allen <137816906+cateallen@users.noreply.github.com>
2024-10-15 09:55:54 -0400

    Update visualization.py

--- src/visualization.py
+++ src/visualization.py
@@ -2,22 +2,12 @@
 import pandas as pd
 import numpy as np
 
-display(pd.crosstab(results['Age Group'], results['Gender']))
-display(pd.crosstab(results['Age Group'], [results['Gender'], results['BQ Pace']]))
-
 # Create a histogram of the number of runners by minimum temp
 subset = results[['Bib', 'Min']].copy()  # Create a copy to avoid SettingWithCopyWarning
 subset['Min'] = subset['Min'].round(0).astype(int)  # Round and convert to int
 
 # Group by 'Min' and count the number of 'Bib' entries
 histo = subset.groupby('Min')['Bib'].count().reset_index(name='Finishers')
-
-# Display the histogram DataFrame
-display(histo)
-
-# Is there a correlation between time to complete the first half and the percent change in pace?
-display(results[['First Half', 'Percent Change']].corr().iloc[0::2,-1])
-
 
 import pandas as pd
 from tabulate import tabulate  # Ensure you have this import
@@ -40,9 +30,6 @@ corrs['Gender'] = corrs['Gender'].replace({'M': 'Men', 'F': 'Women'})
 # Pivot the DataFrame
 corrs_pivot = corrs.pivot(index='Age Group', columns='Gender', values='Percent Change')
 
-# Print the pivoted DataFrame using tabulate
-print(tabulate(corrs_pivot, headers='keys', tablefmt='psql'))
-
 # Is there a correlation between the min temp and the change in pace?
 # subset = results.loc[(results['First Half'] < 5400)]
 corrs = results.groupby(['Gender', 'Age Group'])[['Min', 'Percent Change']].corr().reset_index()
@@ -51,9 +38,6 @@ corrs = corrs.loc[~corrs['Age Group'].isin(['70-74', '75-79', '80 and Over'])]
 corrs['Percent Change'] = corrs['Percent Change'].round(3)
 corrs['Gender'] = corrs['Gender'].replace({'M' : 'Men', 'F' : 'Women'})
 corrs = pd.pivot(data=corrs, index='Age Group', columns='Gender', values='Percent Change')
-
-print(tabulate(corrs, headers='keys', tablefmt='psql'))
-
 
 # Is there a correlation between the max temp and the change in pace?
 corrs = results.groupby(['Gender', 'Age Group'], observed=False)[['Max', 'Percent Change']].corr().reset_index()
@@ -73,9 +57,6 @@ corrs['Gender'] = corrs['Gender'].replace({'M': 'Men', 'F': 'Women'})
 # Pivot the DataFrame
 corrs = pd.pivot(data=corrs, index='Age Group', columns='Gender', values='Percent Change')
 
-# Print the pivoted DataFrame using tabulate
-print(tabulate(corrs, headers='keys', tablefmt='psql'))
-
 
 # Is there a correlation between mean temperature and the change in pace?
 corrs = results.groupby(['Gender', 'Age Group'], observed=False)[['Mean', 'Percent Change']].corr().reset_index()
@@ -90,7 +71,6 @@ corrs = corrs.loc[~corrs['Age Group'].isin(['70-74', '75-79', '80 and Over'])]
 corrs = pd.pivot(data=corrs, index='Age Group', columns='Gender', values='Percent Change')
 
 # Print the pivoted DataFrame using tabulate
-print(tabulate(corrs, headers='keys', tablefmt='psql'))
 
 # How does the median change vary with gender, age group, and min temp?
 median = results.groupby(['Gender', 'Age Group', 'Min Bins'], observed=False)['Percent Change'].median().reset_index()
@@ -103,19 +83,14 @@ median['Percent Change'] = median['Percent Change'].round(3)
 
 # Print the output for men
 men = pd.pivot(median.loc[median['Gender'] == 'M'], index='Age Group', columns='Min Bins', values='Percent Change')
-print(tabulate(men, headers='keys', tablefmt='psql'))
-print()
 
 # Print the output for women
 women = pd.pivot(median.loc[median['Gender'] == 'F'], index='Age Group', columns='Min Bins', values='Percent Change')
-print(tabulate(women, headers='keys', tablefmt='psql'))
-print()
 
 # Prepare the data for an output file to create a visualization
 output = pd.pivot(median, index=['Gender', 'Age Group'], columns='Min Bins', values='Percent Change').reset_index()
 output['Gender'] = output['Gender'].replace({'M': 'Men', 'F': 'Women'})
 
-print(tabulate(output, headers='keys', tablefmt='psql'))
 
 # Filter to only show men under 35 and export data for scatter plot
 subset = results.loc[(results['Gender'] == 'M') & (results['Age Group'] == 'Under 35')]
@@ -135,10 +110,7 @@ subset['Percent Bins'] = pd.cut(subset['Percent Change'], bins, labels=values, i
 # Apply observed=False to groupby
 output = subset.groupby('Percent Bins', observed=False)['Bib'].count().reset_index(name='Finishers')
 
-display(output)
-
 subset = results.groupby(['BQ Pace', 'Gender', 'Min Bins'], observed=False)['Percent Change'].median().reset_index(name='Change')
-display(pd.pivot(subset, index='Min Bins', columns=['Gender', 'BQ Pace'], values='Change'))
 
 corrs = results.groupby(['Gender', 'BQ Pace'])[['First Half', 'Percent Change']].corr().reset_index()
 corrs = corrs.loc[corrs['level_2'] == 'First Half']
@@ -146,12 +118,9 @@ corrs['Percent Change'] = corrs['Percent Change'].round(3)
 corrs['Gender'] = corrs['Gender'].replace({'M' : 'Men', 'F' : 'Women'})
 corrs = pd.pivot(data=corrs, index='BQ Pace', columns='Gender', values='Percent Change')
 
-print(tabulate(corrs, headers='keys', tablefmt='psql'))
 
 corrs = results.groupby(['Gender', 'BQ Pace'])[['Min', 'Percent Change']].corr().reset_index()
 corrs = corrs.loc[corrs['level_2'] == 'Min']
 corrs['Percent Change'] = corrs['Percent Change'].round(3)
 corrs['Gender'] = corrs['Gender'].replace({'M' : 'Men', 'F' : 'Women'})
 corrs = pd.pivot(data=corrs, index='BQ Pace', columns='Gender', values='Percent Change')
-
-print(tabulate(corrs, headers='keys', tablefmt='psql'))
